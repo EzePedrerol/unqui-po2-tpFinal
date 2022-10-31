92ec1bc4559b0d106cc3fe5afcb6be53791c578b
Ezequiel Pedrerol <ezequielpedrerol2002@gmail.com>
2022-10-30 22:44:14 -0300

    Agregué algunos atributos de desafio

--- src/ar/edu/unq/edu/poo2/TPFINAL/Desafio.java
+++ src/ar/edu/unq/edu/poo2/TPFINAL/Desafio.java
@@ -2,4 +2,47 @@ package ar.edu.unq.edu.poo2.TPFINAL;
 
 public class Desafio {
 
+	private int cantidadMuestras;
+	private int dificultad;
+	private int recompensa;
+	private Area area;
+	
+	public Desafio(int cantidadMuestras, int dificultad, int recompensa, Area area) {
+		this.setCantidadMuestras(cantidadMuestras);
+		this.setDificultad(dificultad);
+		this.recompensa(recompensa);
+		this.setArea(area);
+	}
+	
+	public int getCantidadMuestras() {
+		return cantidadMuestras;
+	}
+	
+	public void setCantidadMuestras(int muestras) {
+		this.cantidadMuestras = muestras;
+	}
+	
+	public int getDificultad() {
+		return dificultad;
+	}
+	
+	public void setDificultad(int dificultad) {
+		this.dificultad = dificultad;
+	}
+	
+	public int getRecompensa() {
+		return recompensa;
+	}
+	
+	public void setRecompensa(int recompensa) {
+		this.recompensa = recompensa;
+	}
+	
+	public Area getArea() {
+		return area;
+	}
+	
+	public void setArea(Area area) {
+		this.area = area;
+	}
 }
