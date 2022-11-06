ee8bb01fee6e1e298c943b6c23ce1ff0e4e2af87
Ezequiel Pedrerol <ezequielpedrerol2002@gmail.com>
2022-11-06 19:44:55 -0300

    agrega favoritos

--- src/ar/edu/unq/edu/poo2/TPFINAL/Favoritos.java
+++ src/ar/edu/unq/edu/poo2/TPFINAL/Favoritos.java
@@ -1,5 +1,12 @@
 package ar.edu.unq.edu.poo2.TPFINAL;
 
-public class Favoritos {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Favoritos implements Recomendacion {
+
+	@Override
+	public List<Desafio> elegir(Desafio desafio) {
+		
+	}
 }
